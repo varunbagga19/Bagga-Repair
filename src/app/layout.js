8ec91a1a0fd00122a804d94e1bd1144985aacfe4
varunbagga19 <varunbagga19@gmail.com>
2023-08-19 12:38:36 +0530

    NavBar made responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import NavBar from '@/components/NavBar'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,26 @@
-import React from 'react'
-
+"use client";
+import React, { use } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 function NavBar() {
+    const [toggle, setToggle] = useState(true);
+    const [iconName,setIcon] = useState(faBars)
+    function handleToggle(e){
+        e.preventDefault();
+        setToggle(!toggle); // Toggle the value
+        setIcon(toggle ? faBars : faTimes); // Toggle the icon
+    }
+
   return (
     <header className='bg-white font-serif'>
-       <nav className='flex justify-between items-center w-[92%] mx-auto py-3'>
+       <nav className='flex justify-between items-center w-[92%] mx-auto py-2'>
             <div>
                 <h1 className='text-black font-bold text-xl'>Bagga Repair</h1>
             </div>
-            <div className='text-black'>
-            <ul className='flex items-center gap-[4vw]'>
+            <div className={`text-black md:static absolute bg-white md:min-h-fit min-h-[60vh] md:w-auto left-0 ${toggle ? 'top-[9%]':'top-[-100%]' } w-full my-5 flex items-center px-5`}>
+            <ul className='flex md:flex-row flex-col items-center md:gap-[4vw] gap-8'>
                 <li>
                     <a className='hover:text-gray-500'>Products</a>
                 </li>
@@ -26,8 +38,9 @@ function NavBar() {
                 </li>
             </ul>
             </div>
-            <div>
-                <button class='bg-[#a6c1ee] tex-white px-5 py-2 rounded-full hover:bg-[#87acec]'>Sign in</button>
+            <div className='flex items-center gap-6'>
+                <button class='bg-[#a6c1ee] tex-white px-5 py-2 min-w- rounded-full hover:bg-[#87acec]'>Sign in</button>
+                <FontAwesomeIcon onClick={handleToggle} icon={iconName} className='text-3xl cursor-pointer md:hidden' style={{color: "#000205",}} />
             </div>
        </nav>
 
@@ -35,4 +48,4 @@ function NavBar() {
   )
 }
 
-export default NavBar
+export default NavBar;
